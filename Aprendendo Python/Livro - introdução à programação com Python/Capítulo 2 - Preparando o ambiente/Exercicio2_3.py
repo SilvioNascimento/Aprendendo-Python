323aa262c9a726735ce41d0cdd680d1967f24c42
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-05-20 23:37:33 -0300

    Adicionando a resolução dos exercícios do capítulo 2 - parte final

---
+++ Aprendendo Python/Livro - introdução à programação com Python/Capítulo 2 - Preparando o ambiente/Exercicio2_3.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que exiba seu nome na tela
+"""
+
+print("Olá, Silvio!")
---
+++ Aprendendo Python/Livro - introdução à programação com Python/Capítulo 2 - Preparando o ambiente/Exercicio2_5.py
@@ -0,0 +1,13 @@
+"""
+Modifique o primeiro programa, listagem 2.7, de forma a calcular a soma de três variáveis
+
+Listagem 2.7:
+    a = 2
+    b = 3
+    print(a + b)
+"""
+
+a = 2
+b = 3
+c = 5
+print(a + b + c)
