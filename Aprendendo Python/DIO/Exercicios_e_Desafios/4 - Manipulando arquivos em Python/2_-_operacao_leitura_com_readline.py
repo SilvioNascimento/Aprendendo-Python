8d926d4d0005e2429afabf2a42994a6f9147a2c3
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-07-03 23:40:30 -0300

    Realizando exercícios e desafio sobre a manipulação de arquivos em Python

---
+++ Aprendendo Python/DIO/Exercicios_e_Desafios/4 - Manipulando arquivos em Python/2_-_operacao_leitura_com_readline.py
@@ -0,0 +1,12 @@
+file = open('lorem.txt', 'r')
+
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+
+file.close()
