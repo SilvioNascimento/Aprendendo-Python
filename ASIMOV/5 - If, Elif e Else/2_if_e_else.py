1723a358600a1eebb25d48f9a5598277d98a8837
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 09:04:39 -0300

    feat: adicionando um código que usa estrutura de condição if/else

---
+++ ASIMOV/5 - If, Elif e Else/2_if_e_else.py
@@ -0,0 +1,14 @@
+# Este código é apenas a estrutura de condição IF/ELSE
+
+idade = 18
+possui_carteira = False
+
+# Se a idade for maior ou igual a 18 e possuir carteira, então execute o bloco de código
+if (idade >= 18) and possui_carteira:
+    print("Você pode dirigir.")
+    a = 10
+    print(f"Valor teste: {a}")
+
+# Caso a condição acima não for atendida, então execute o bloco de código abaixo
+else:
+    print("Você não pode dirigir.")
