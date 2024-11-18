c03a51ea4b4caf6dac10fcb641b816b06e1a00d6
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 15:28:59 -0300

    feat: exibindo a tabela de reviews

--- ASIMOV/1 - Python para iniciantes_do zero ao primeiro projeto/7 - Projeto/app.py
+++ ASIMOV/1 - Python para iniciantes_do zero ao primeiro projeto/7 - Projeto/app.py
@@ -1,3 +1,11 @@
 import streamlit as st      # importa a biblioteca que transforma scripts de dados para aplicativos da web
+import pandas as pd         # importa a biblioteca que trabalha com manipulação e análises de dados
 
-st.write("Olá, mundo!")
+# Estica a tabela de dados
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("./datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("./datasets/Top-100 Trending Books.csv")
+
+# Exibe a tabela de dados sobre reviews
+df_reviews
