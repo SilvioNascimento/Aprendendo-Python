ab6490b5a280e9756d6f2f00b7e413c99e5db878
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 10:22:59 -0300

    refactor: movendo os códigos para outra pasta

--- ASIMOV/1 - Hello world_numeros_e_REPL/1_ola_mundo.py
+++
@@ -1,3 +0,0 @@
-# Este código é apenas o início da aprendizagem
-# Apenas mostra a mensagem 'Olá, mundo' no terminal
-print("Olá, mundo!")
---
+++ ASIMOV/1 - Python para iniciantes_do zero ao primeiro projeto/1 - Hello world_numeros_e_REPL/1_ola_mundo.py
@@ -0,0 +1,3 @@
+# Este código é apenas o início da aprendizagem
+# Apenas mostra a mensagem 'Olá, mundo' no terminal
+print("Olá, mundo!")
