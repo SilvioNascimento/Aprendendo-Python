ac87f4f4347c0478515b03f194239e291f1d1ffd
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-17 23:30:04 -0300

    feat: adicionando um código que utiliza strings e variaveis

---
+++ ASIMOV/2 - Strings e variáveis/1_strings_e_variaveis.py
@@ -0,0 +1,12 @@
+texto = "String"        # Esta variável armazena um valor do tipo string
+print(type(texto))      # Saída: <class 'str'>
+
+
+# Concatenação de strings
+valor1 = "Olá, "
+valor2 = "Mundo"
+frase1 = valor1 + valor2
+print(frase1)
+
+frase2 = "Oi" + "1"     # Devem ser do mesmo tipo para poder concatenar
+print(frase2)
