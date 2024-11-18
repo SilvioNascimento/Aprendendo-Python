fefed1cfed5ef3d31902882dd96ea6bbae4694be
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 00:07:47 -0300

    feat: adicionando um código que usa a formatação em string

---
+++ ASIMOV/3 - Formatação de strings, listas e slicing/1_formatacao_em_string.py
@@ -0,0 +1,9 @@
+# Este código utiliza a formatação de strings através de 'f string'
+# f string: permite mostrar valores de variáveis dentro de uma string
+
+a = "Silvio"
+b = 22
+c = "LCC"
+
+frase = f"Olá!\nAqui quem fala é {a}, tenho {b} anos de idade e estou graduando em {c}"
+print(frase)
