f1c66cf400e84d35ecefbf6971fb5a1968223564
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 17:08:20 -0300

    feat: adicionando um slider dentro de uma sidebar que filtra dados na tabela

--- ASIMOV/1 - Python para iniciantes_do zero ao primeiro projeto/7 - Projeto/app.py
+++ ASIMOV/1 - Python para iniciantes_do zero ao primeiro projeto/7 - Projeto/app.py
@@ -7,8 +7,15 @@ st.set_page_config(layout="wide")
 df_reviews = pd.read_csv("./datasets/customer reviews.csv")
 df_top100_books = pd.read_csv("./datasets/Top-100 Trending Books.csv")
 
+# Procura o maior preço dos livros top 100
+price_max = df_top100_books["book price"].max()
 
+# Procura o menor preço dos livros top 100
+price_min = df_top100_books["book price"].min()
 
+# Adicionando um slider dentro de um sidebar de streamlit
+max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
 
-
-
+# Filtra os dados que será exibido na tabela pelo 'max_price'
+df_books = df_top100_books[df_top100_books["book price"] <= max_price]
+df_books
