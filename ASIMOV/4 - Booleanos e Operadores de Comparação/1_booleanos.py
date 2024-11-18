9b9b7c3db044a0dbc8b99f3f42151841f70ffd4e
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 08:09:51 -0300

    feat: adicionando um código que usa booleanos

---
+++ ASIMOV/4 - Booleanos e Operadores de Comparação/1_booleanos.py
@@ -0,0 +1,11 @@
+# Este código irá trabalhar com valores booleanos
+
+# Criando variáveis que armazenarão True e False, respectivamente
+true = True
+false = False
+
+# Verificando o tipo dos valores das variáveis acima
+print(type(true))       # Saída: <class 'bool'>
+print(type(false))      # Saída: <class 'bool'>
+
+# Os valores booleanos terão mais importância quando usados nos operadores de comparação
