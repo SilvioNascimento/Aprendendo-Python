b85d1504701519a99979c2f6bf5a2e8595de66a1
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-17 21:49:50 -0300

    feat: adicionando um código que mostra os tipos de valores

---
+++ ASIMOV/1 - Hello world_numeros_e_REPL/3_verificar_tipo_numeros.py
@@ -0,0 +1,4 @@
+# Este código apenas mostra o tipo dos números, que são 'inteiros' (int) e 'flutuantes' (float)
+
+print(type(32))     # Saída: <class 'int'>
+print(type(32.5))   # Saída: <class 'float'>
