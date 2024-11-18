f2241e4ad371298878f55e4783f5253823c29f73
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-18 09:04:31 -0300

    feat: adicionando um código que usa estrutura de condição if

---
+++ ASIMOV/5 - If, Elif e Else/1_if.py
@@ -0,0 +1,11 @@
+# Este código é apenas a estrutura de condição IF
+
+idade = 18
+
+# Se a idade for maior ou igual a 18, então execute o bloco de código
+if (idade >= 18):
+    print("Você pode dirigir.")
+    a = 10
+    print(f"Valor teste: {a}")
+
+print("Fim do programa")
